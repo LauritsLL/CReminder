cd2d00cfac0f7384aa64db02704244429cdd5ec0
Lause132 <lll2005@live.dk>
2020-04-09 21:01:51 +0200

    Fixed sleep() function in thread so it actually works.

--- reminder.c
+++ reminder.c
@@ -34,6 +34,20 @@ struct checkReminderArgs {
     int *n;
 };
 
+void sleep_ms(int milliseconds) // cross-platform sleep function
+{
+    #ifdef WIN32
+        Sleep(milliseconds);
+    #elif _POSIX_C_SOURCE >= 199309L
+        struct timespec ts;
+        ts.tv_sec = milliseconds / 1000;
+        ts.tv_nsec = (milliseconds % 1000) * 1000000;
+        nanosleep(&ts, NULL);
+    #else
+        usleep(milliseconds * 1000);
+    #endif
+}
+
 void checkMemoryAlloc(void *pointer) {
     if (!pointer) {
         printf("Memory Allocation failed! Aborting...\n");
@@ -112,7 +126,7 @@ void* checkReminders(void *arguments) {
             }
         }
         // Sleep.
-        sleep(.9);
+        sleep_ms(900);
     }
     return NULL;
 }
